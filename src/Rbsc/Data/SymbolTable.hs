650dfd5fd1db98c8afd5285387bda16645a857cf
Philipp Chrszon <Philipp.Chrszon@tu-dresden.de>
2018-02-08 15:41:12 +0100

    Implement adding of constants into symbol table

--- src/Rbsc/Data/SymbolTable.hs
+++ src/Rbsc/Data/SymbolTable.hs
@@ -6,6 +6,8 @@
 module Rbsc.Data.SymbolTable
     ( SymbolTable
     , fromModel
+
+    , fromSyntaxType
     ) where
 
 
@@ -23,15 +25,20 @@ import Rbsc.Data.Type
 import qualified Rbsc.Report.Error.Syntax as Syntax
 import           Rbsc.Report.Region       (Loc (..), Region)
 
+import           Rbsc.Syntax.Constant
 import           Rbsc.Syntax.Expr.Untyped
 import           Rbsc.Syntax.Model        (Model)
 import qualified Rbsc.Syntax.Model        as Model
+import qualified Rbsc.Syntax.Type         as Syntax
 
 
 -- | The symbol table holds the type of each identifier in the model
 -- source.
 type SymbolTable = Map Name SomeType
 
+
+-- | The @BuilderState@ holds the location-annotated symbol table and the
+-- list of errors encountered so far.
 data BuilderState = BuilderState
     { _symbols :: Map Name (SomeType, Region)
     , _errors  :: [Syntax.Error]
@@ -42,8 +49,9 @@ makeLenses ''BuilderState
 
 -- | Extract a 'SymbolTable' from a 'Model'.
 fromModel :: ComponentTypes -> Model -> Either [Syntax.Error] SymbolTable
-fromModel types model =
-    runBuilder (components types (Model.system model))
+fromModel types model = runBuilder $ do
+    constants (Model.constants model)
+    components types (Model.system model)
 
 
 type Builder a = State BuilderState a
@@ -56,14 +64,19 @@ runBuilder m =
         else Left errs
 
 
+constants :: [ConstantDef] -> Builder ()
+constants defs = for_ defs $ \(ConstantDef (Loc name rgn) sTy _) ->
+    insert name (fromSyntaxType sTy) rgn
+
+
 -- | Add component instances defined within the system block to the symbol
 -- table.
 components :: ComponentTypes -> [Loc Expr] -> Builder ()
 components types es = for_ es $ \case
     Loc (HasType (Loc (Variable name) rgnVar) (Loc tyName rgnTy)) _ ->
         lookupComponentType types tyName rgnTy >>= \case
-            Just aTy -> insert name aTy rgnVar
-            Nothing  -> return ()
+            Just ty -> insert name ty rgnVar
+            Nothing -> return ()
     _ -> return ()
 
 
@@ -82,11 +95,22 @@ lookupComponentType types tyName rgn
 -- | Insert an identifier with a given 'Name' and 'SomeType' into the symbol
 -- table. Throws an error if the identifier has been defined already.
 insert :: Name -> SomeType -> Region -> Builder ()
-insert name aTy rgn =
+insert name ty rgn =
     use (symbols.at name) >>= \case
         Just (_, rgnFirst) -> throw (Syntax.DuplicateIdentifier rgn rgnFirst)
-        Nothing -> symbols.at name .= Just (aTy, rgn)
+        Nothing -> symbols.at name .= Just (ty, rgn)
 
 
 throw :: Syntax.Error -> Builder ()
 throw e = modifying errors (++ [e])
+
+
+-- | Convert the syntactical representation of a type into the internal
+-- type representation.
+fromSyntaxType :: Syntax.Type -> SomeType
+fromSyntaxType = \case
+    Syntax.TyBool         -> SomeType TyBool
+    Syntax.TyInt          -> SomeType TyInt
+    Syntax.TyDouble       -> SomeType TyDouble
+    Syntax.TyArray elemTy -> case fromSyntaxType elemTy of
+        SomeType ty -> SomeType (TyArray ty Nothing)
